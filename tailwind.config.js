79e8d13950a321c73b5723ed415a2b60ec0f0a96
Eldi <82001023+eldimindcrafter123@users.noreply.github.com>
2023-02-24 18:26:47 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/logo.png')",
+          'footer-texture': "url('/img/logo.png')",
+        },
+        spacing: {
+          '5px': '5px',
+        }
+      }
+    }
+  }
